3a7affc3699128e1299e1a6548d64050f262055b
luisscrc <32582552+luisscrc@users.noreply.github.com>
2017-11-09 10:03:46 -0600

    Add files via upload
    Version mejorada del sistema de humedad

--- problemadeaplicacion2.cpp
+++ problemadeaplicacion2.cpp
@@ -1,6 +1,6 @@
 /*Este programa identifica un tipo de ciclo para una variable asignada (fruta)
-en cada caso llegará a un límite para establecer una temperatura establecida */
-/*Intervalos 
+en cada caso llegará a un límite para establecer una temperatura establecida 
+Intervalos de humedad
 tomate 50-60%
 melon 60-70%
 pepino 70-90%*/
@@ -12,38 +12,57 @@ int fruta;
 float temperatura;
 printf("Para estabilizar la humedad primero escoja una fruta\nPara tomate presione 1, \nPara melon presione 2, \nPara papino persione 3\n");
 scanf("%d",&fruta);
-if(fruta=1){ //tomate 
+if(fruta==1){ //tomate 50-60
      printf("Ingrese la humedad en este momento\n");
      scanf("%f",&temperatura);
-            for(temperatura<50;temperatura>60;temperatura++){
+     if(temperatura<50){
+		for (temperatura=temperatura;temperatura<50;temperatura++){
+			temperatura=temperatura+(temperatura*0.04);
+			printf("Regando a fin de aumentar la humedad %f %\n",temperatura); // Escribe el número actual de la 'cuenta regresiva'
+            Sleep (3000); // Espera diez segundos
+		}			  }
+        else if (temperatura>60){
+		   for(temperatura<50;temperatura>60;temperatura++){
             temperatura=temperatura-(temperatura*0.03);
-        	printf("La temperatura es %f %\n",temperatura); // Escribe el número actual de la 'cuenta regresiva'
+        	printf("Abriendo ventilacion para bajar la humedad... Ahora es %f grados\n",temperatura); // Escribe el número actual de la 'cuenta regresiva'
             Sleep (10000); // Espera diez segundos
-            }
+            }}
             printf("Humedad necesaria para cosechar tomates");
         	return 0;
             }
-else if (fruta=2) { //Melon
-printf("Ingrese la humedad en este momento\n");
+else if (fruta==2) { //Melon 60-70
+	 printf("Ingrese la humedad en estos momentos\n");
      scanf("%f",&temperatura);
+	 if(temperatura<60){
+		for (temperatura=temperatura;temperatura<60;temperatura++){
+			temperatura=temperatura+(temperatura*0.04);
+			printf("Regando a fin de aumentar la humedad %f %\n",temperatura); // Escribe el número actual de la 'cuenta regresiva'
+            Sleep (3000); // Espera diez segundos
+					}  }
+        else if (temperatura>70){
             for(temperatura<60;temperatura>70;temperatura++){
             temperatura=temperatura-(temperatura*0.03);
-        	printf("La temperatura es %f %\n",temperatura); // Escribe el número actual de la 'cuenta regresiva'
+        	printf("Abriendo ventilacion para bajar la humedad... Ahora es %f grados\n",temperatura); // Escribe el número actual de la 'cuenta regresiva'
             Sleep (10000); // Espera diez segundos
-            }
+            }}
             printf("%f Humedad necesaria para cosechar melones", temperatura);
         	return 0;
             }    
- 				
-else if (fruta=3) { //pepino
-printf("Ingrese la humedad en este momento\n");
+else if (fruta==3) { //pepino 70-90
+	 printf("Ingrese la humedad en este momento\n");
      scanf("%f",&temperatura);
+     if(temperatura<70){
+		for (temperatura=temperatura;temperatura<60;temperatura++){
+			temperatura=temperatura+(temperatura*0.04);
+			printf("Regando a fin de aumentar la humedad %f %\n",temperatura); // Escribe el número actual de la 'cuenta regresiva'
+            Sleep (3000); // Espera diez segundos
+					}  }
+        else if (temperatura>90){
             for(temperatura<70;temperatura>90;temperatura++){
             temperatura=temperatura-(temperatura*0.03);
-        	printf("La temperatura es %f %\n",temperatura); // Escribe el número actual de la 'cuenta regresiva'
+        	printf("Abriendo ventilacion para bajar la humedad... Ahora es %f grados\n",temperatura); // Escribe el número actual de la 'cuenta regresiva'
             Sleep (10000); // Espera un segundo
-            }
-            printf("%f Humedad necesaria para cosechar tomates",temperatura);
+            }}
+            printf("%f Humedad necesaria para cosechar pepinos",temperatura);
         	return 0;
-            }      
- 				}     
+            }      }//Fin de main     
